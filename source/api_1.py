aa054405ac219a85e64ff59824d51c18d538f69c
Technik Tüftler <technik.tueftler@gmail.com>
2022-05-11 22:21:45 +0200

    save tests for twitter api v1 and add github action for pylint

--- source/api_1.py
+++ source/api_1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import tweepy
 import os
 
@@ -8,25 +10,30 @@ auth = tweepy.OAuth1UserHandler(
     os.getenv("access_token_secret"))
 api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    """messages = api.get_direct_messages(count=5)
-    print(messages)"""
-    for page in tweepy.Cursor(api.get_direct_messages, count=20).pages(1):
-        for message in page:
-            print(message.created_timestamp)
-            print(message.id)
-            print(message.message_create["message_data"])
-            print(message.message_create["message_data"]["text"])
-            print(message.message_create["message_data"]["entities"]["urls"][0]["expanded_url"])
 
-            # re für "bot" --> ^#?bot\s(.*)
-            # codeschmiede --> ^(#bot)\s*(.*)$
-            # Luni --> ^(#bot)($|\s)(.*)$ oder ^(?<tag>#[\w\d]+)($|\s+)(?<msg>.*)$
+def message_handler():
+    try:
+        api.verify_credentials()
+        for page in tweepy.Cursor(api.get_direct_messages, count=20).pages(1):
+            for message in page:
+                print(message.created_timestamp)
+                print(message.id)
+                print(message.message_create["message_data"])
+                print(message.message_create["message_data"]["text"])
+                #print(message.message_create["message_data"]["entities"]["urls"][0]["expanded_url"])
+                print(message.message_create["message_data"]["entities"]["urls"])
 
-except tweepy.TooManyRequests:
-    print(tweepy.TooManyRequests.response)
-except tweepy.Unauthorized:
-    print("Anmeldedaten nicht korrekt")
-except tweepy.errors.Forbidden as err:
-    print(f"Keinen vollen Zugang: {err}")
+                # re für "bot" --> ^#?bot\s(.*)
+                # codeschmiede --> ^(#bot)\s*(.*)$
+                # Luni --> ^(#bot)($|\s)(.*)$ oder ^(?<tag>#[\w\d]+)($|\s+)(?<msg>.*)$
+
+    except tweepy.TooManyRequests:
+        print(tweepy.TooManyRequests.response)
+    except tweepy.Unauthorized:
+        print("Anmeldedaten nicht korrekt")
+    except tweepy.errors.Forbidden as err:
+        print(f"Keinen vollen Zugang: {err}")
+
+
+if __name__ == "__main__":
+    message_handler()
