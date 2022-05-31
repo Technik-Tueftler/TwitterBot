1e8675086b679a9c1280dadc8a4f9020ccb3dfa1
Technik Tüftler <technik.tueftler@gmail.com>
2022-05-31 23:14:25 +0200

    - change TIMESTAMP to DateTime format that time would be saved in UTC. - finish the first implementation for store data

--- source/message_handler.py
+++ source/message_handler.py
@@ -8,6 +8,7 @@ function should run later in the docker container.
 import os
 import time
 import datetime
+from dataclasses import dataclass
 import re
 import schedule
 import tweepy
@@ -17,8 +18,16 @@ MESSAGE_PATTERN = r"^#?bot\s(.*)\s(https:.*)"
 # MESSAGE_PATTERN = r"^#?tweet\s(.*)\s(https:.*)"
 # MESSAGE_PATTERN = r"^#?user\s(.*)\s(https:.*)"
 TWEET_URL_PATTERN = r"^(https://twitter.com/)(.+)(/status/)(\d+)$"
-MAX_TWEETS_PER_PAGE = 1
+MAX_TWEETS_PER_PAGE = 10
 MAX_PAGES = 1
+
+
+@dataclass
+class TwitterMessage:
+    message_id: str
+    message_timestamp: datetime.datetime
+    sender_id: str
+    message_data: dict
 
 
 def decompose_tweet_url(tweet_url: str) -> dict:
@@ -27,9 +36,7 @@ def decompose_tweet_url(tweet_url: str) -> dict:
     :param tweet_url: Complete url of the tweet in a str
     :return: Dictionary with information with username, tweet id and if results were found.
     """
-    result_return = {"found_match": False,
-                     "twitter_user_name": None,
-                     "tweet_id": None}
+    result_return = {"found_match": False, "twitter_user_name": None, "tweet_id": None}
     result = re.match(TWEET_URL_PATTERN, tweet_url)
     if result is not None:
         result_return["twitter_user_name"] = result.groups()[1]
@@ -44,9 +51,7 @@ def analyze_message(tweet_text: str) -> dict:
     :param tweet_text: Complete message in a str
     :return: Dictionary with information with message, url and if results were found.
     """
-    result_return = {"found_match": False,
-                     "message": None,
-                     "short_url": None}
+    result_return = {"found_match": False, "message": None, "short_url": None}
     result = re.match(MESSAGE_PATTERN, tweet_text)
     if result is not None:
         result_return["message"] = result.groups()[0]
@@ -80,14 +85,19 @@ def get_all_matched_messages(api_endpoint: tweepy.API) -> list:
     """
     Get all messages from twitter and check if a match found.
     :param api_endpoint: Twitter api endpoint
-    :return: list with content of the messages
+    :return: list with messages
     """
-    matched_messages = [message.message_create["message_data"]
-                        for page in tweepy.Cursor(api_endpoint.get_direct_messages,
-                                                  count=MAX_TWEETS_PER_PAGE).pages(MAX_PAGES)
-                        for message in page
-                        if analyze_message(
-            message.message_create["message_data"]["text"])["found_match"]]
+    matched_messages = [
+        TwitterMessage(message_id=message.id, message_timestamp=message.created_timestamp,sender_id=message.message_create["sender_id"], message_data=message.message_create["message_data"])
+        #  message.message_create["message_data"]
+        for page in tweepy.Cursor(
+            api_endpoint.get_direct_messages, count=MAX_TWEETS_PER_PAGE
+        ).pages(MAX_PAGES)
+        for message in page
+        if analyze_message(message.message_create["message_data"]["text"])[
+            "found_match"
+        ]
+    ]
     return matched_messages
 
 
@@ -98,12 +108,13 @@ def get_tweet_status(api: tweepy.API, tweet_id: str) -> dict:
     :param tweet_id:
     :return:
     """
-    tweet_status_data = {"author_user_id": None,
-                         "author_user_name": None,
-                         "author_user_screen_name": None,
-                         "created_at": None,
-                         "text": None
-                         }
+    tweet_status_data = {
+        "author_user_id": None,
+        "author_user_name": None,
+        "author_user_screen_name": None,
+        "created_at": None,
+        "text": None,
+    }
     try:
         tweet_status = api.get_status(tweet_id)
         tweet_status_data["author_user_id"] = tweet_status.user.id
@@ -128,19 +139,28 @@ def message_handler(communication_data: dict) -> None:
         communication_data["consumer_key"],
         communication_data["consumer_secret"],
         communication_data["access_token"],
-        communication_data["access_token_secret"])
+        communication_data["access_token_secret"],
+    )
     api = tweepy.API(auth, wait_on_rate_limit=True)
     try:
         api.verify_credentials()
         results_message = get_all_matched_messages(api)
         for message_content in results_message:
-            tweet_data = {"comment": analyze_message(message_content["text"])["message"],
-                          "expand_url": extract_expand_url(message_content)}
-            tweet_id = decompose_tweet_url(tweet_data["expand_url"])["tweet_id"]
-            tweet_data["tweet_id"] = tweet_id
-            tweet_data |= get_tweet_status(api, tweet_id)
-            db.add_tweet(tweet_data)
+            tweet_data = {
+                "comment": analyze_message(message_content.message_data["text"])["message"],
+                "expand_url": extract_expand_url(message_content.message_data),
+            }
+            decomposed_url = decompose_tweet_url(tweet_data["expand_url"])
+            tweet_data["tweet_id"] = decomposed_url["tweet_id"]
+            tweet_data |= get_tweet_status(api, decomposed_url["tweet_id"])
             print(tweet_data)
+            if tweet_data["author_user_id"] is not None:
+                db.add_tweet(tweet_data)
+                db.check_and_update_user_names(tweet_data)
+            else:
+                db.add_deleted_tweet(
+                    decomposed_url, tweet_data["expand_url"], tweet_data["comment"]
+                )
 
     except tweepy.TooManyRequests as err:
         print(err)
@@ -152,19 +172,25 @@ def message_handler(communication_data: dict) -> None:
 
 def check_and_verify_env_variables() -> dict:
     """Function controls the passed env variables and checks if they are valid."""
-    environment_data = {"consumer_key": os.getenv("consumer_key"),
-                        "consumer_secret": os.getenv("consumer_secret"),
-                        "access_token": os.getenv("access_token"),
-                        "access_token_secret": os.getenv("access_token_secret"),
-                        "all_verified": True}
+    environment_data = {
+        "consumer_key": os.getenv("consumer_key"),
+        "consumer_secret": os.getenv("consumer_secret"),
+        "access_token": os.getenv("access_token"),
+        "access_token_secret": os.getenv("access_token_secret"),
+        "all_verified": True,
+    }
 
     if any(True for value in environment_data.values() if value is None):
         environment_data["all_verified"] = False
-        print("Not all env variable are defined. Please check the documentation and add all twitter"
-              "authentication information.")
+        print(
+            "Not all env variable are defined. Please check the documentation and add all twitter"
+            "authentication information."
+        )
     try:
         _ = datetime.datetime.strptime(os.getenv("schedule_time_every_day"), "%H:%M")
-        environment_data["schedule_time_every_day"] = os.getenv("schedule_time_every_day")
+        environment_data["schedule_time_every_day"] = os.getenv(
+            "schedule_time_every_day"
+        )
         environment_data["all_verified"] &= True
 
     except ValueError:
@@ -183,7 +209,9 @@ def main(env_data: dict) -> None:
     :param env_data: Dictionary with app information.
     :return: None
     """
-    schedule.every().day.at(env_data["schedule_time_every_day"]).do(message_handler, env_data)
+    schedule.every().day.at(env_data["schedule_time_every_day"]).do(
+        message_handler, env_data
+    )
     print("Env data are verified, start job.")
     while True:
         schedule.run_pending()
