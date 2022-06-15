1f3aa0ff228b365b8bb21b7e0520e2f481d0033c
Technik Tüftler <technik.tueftler@gmail.com>
2022-06-15 22:29:25 +0200

    delete tweet and comments if no other link between tweets and comments

--- source/control_db.py
+++ source/control_db.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Console application to run database query's depends on user input
+"""
 
-from source import db
 from rich.console import Console
 from rich.table import Table
+from source import db
 
 
 def close() -> None:
-    pass
+    """
+    Dummy function to close the console app
+    :return: None
+    """
 
 
 def count_all_tweets_from_user() -> None:
+    """
+    Print numbers of tweets for each registered Twitter user and number of renaming.
+    :return: None
+    """
     with db.SQLAlchemyConnectionManager(db.CONNECTOR) as conn:
         users = conn.session.query(db.TwitterUser).all()
         table = Table(title="Übersicht Anzahl von Tweets")
@@ -31,24 +41,52 @@ def count_all_tweets_from_user() -> None:
 
 
 def print_menu(menu: dict) -> None:
+    """
+    Print app menu
+    :param menu: dictionary of registered menu
+    :return: None
+    """
     for key, value in menu.items():
-        print(f"{key:2}: {value}")
+        print(f"{key:>2}: {value}")
 
 
-"""def delete_tweet() -> None:
-    option = int(input('Tweet-ID die gelöscht werden soll: '))
+def delete_tweet() -> None:
+    """
+    Start new user context and delete Tweet based on ID and comment if this is requested.
+    :return: None
+    """
+    input_tweet_id = int(input("Tweet-ID die gelöscht werden soll: "))
     with db.SQLAlchemyConnectionManager(db.CONNECTOR) as conn:
-        conn.session.query()"""
+        input_delete_comments = input("Kommentar auch löschen? (j,n): ")
+        tweet = (
+            conn.session.query(db.Tweet).filter(db.Tweet.id == input_tweet_id).first()
+        )
+        if input_delete_comments == "j":
+            comment_ids_from_tweet = [
+                comment.id for comment in tweet.comments if len(comment.tweets) == 1
+            ]
+        tweet.comments = []
+        conn.session.query(db.Tweet).filter(db.Tweet.id == input_tweet_id).delete(
+            synchronize_session=False
+        )
+        if input_delete_comments == "j":
+            for comment_id in comment_ids_from_tweet:
+                conn.session.query(db.Comment).filter(
+                    db.Comment.id == comment_id
+                ).delete(synchronize_session=False)
+        conn.session.commit()
 
 
 def main() -> None:
-    menu = {1: "Zähle alle Tweets",
-            2: "Beende"}
-    commands = {1: count_all_tweets_from_user,
-                2: close}
+    """
+    Main function to run the console app
+    :return:
+    """
+    menu = {1: "Zähle alle Tweets", 2: "Tweet löschen", 10: "Beende"}
+    commands = {1: count_all_tweets_from_user, 2: delete_tweet, 10: close}
     while True:
         print_menu(menu)
-        option = int(input('Option wählen: '))
+        option = int(input("Option wählen: "))
         command = commands.get(option)
         if command is close:
             break
